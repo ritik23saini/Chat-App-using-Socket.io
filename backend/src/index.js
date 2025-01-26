28026a0bb26de469b1618fb0f095076b09cfff62
ritik23saini <ritik23saini@gmail.com>
2025-01-26 23:41:03 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
-//import { connectDB } from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
- import { seedDatabase } from "./seeds/user.seed.js";
+//import { seedDatabase } from "./seeds/user.seed.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -37,6 +37,6 @@ if (process.env.NODE_ENV === "production") {
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
-  seedDatabase(); //generate dummy data
-  //connectDB();
+  //seedDatabase(); //generate dummy data
+  connectDB();
 });
