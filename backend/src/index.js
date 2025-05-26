7b04bec019910a6d5f535c13286d8b57c749366b
ritik23saini <ritik23saini@gmail.com>
2025-05-26 18:59:15 +0530

    prod 5

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,37 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
+
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-/* import { seedDatabase } from "./seeds/user.seed.js";
- */dotenv.config();
+
+dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      `http://localhost:${PORT}`,
-      "https://test-odn0.onrender.com",
-    ],
-
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-//app.use((_,res) => { res.send("server online!")})
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -45,5 +39,4 @@ if (process.env.NODE_ENV === "production") {
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
-  // seedDatabase(); //generate dummy data
 });
